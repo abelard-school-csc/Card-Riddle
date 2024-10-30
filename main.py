ad14f875f0239349b58ad6c8e0613cea841855d5
AndrewAMur <andrewamuratov@gmail.com>
2024-10-30 16:51:35 -0400

    reduced code size

--- main.py
+++ main.py
@@ -7,46 +7,30 @@ class Card:
     def __init__(self, suit, val):
         self.value = val
         self.suit = suit
-    def show_card(self):
+    def show(self):
         print(self.suit, self.value)
 
-deck = []
+def make_deck():
+    deck = []
 
-for suit in suits:
-    for value in values:
-        new_card = Card(suit, value)
-        deck.append(new_card)
+    for suit in suits:
+        for value in values:
+            new_card = Card(suit, value)
+            deck.append(new_card)
 
-chosen_cards = []
+    return deck
 
-for card in range(5):
-    card = random.choice(deck)
-    deck.remove(card)
-    chosen_cards.append(card)
+def choose_five():
+    deck = make_deck()
+    chosen_cards = []
 
-for i in range(len(chosen_cards)):
-    for j in range(i + 1, len(chosen_cards)):
-        # print(f"First card: {chosen_cards[i].suit}, {chosen_cards[i].value}, Second Card: {chosen_cards[j].suit}, {chosen_cards[j].value}")
+    for card in range(5):
+        card = random.choice(deck)
+        deck.remove(card)
+        chosen_cards.append(card)
 
-        first_suit = chosen_cards[i].suit
-        second_suit = chosen_cards[j].suit
+    return chosen_cards
 
-        if first_suit == second_suit:
-            same_suit = (chosen_cards[i], chosen_cards[j])
 
-print("Same suit:")
-
-for i in same_suit:
-    i.show_card()
-
-removed_card = same_suit[0]
-
-remaining_cards = [same_suit[1]]
-
-for i in chosen_cards:
-    if i not in same_suit:
-        remaining_cards.append(i)
-
-print("Remaning cards:")
-for i in remaining_cards:
-    i.show_card()
+for i in range(5):
+    five_cards = choose_five()
