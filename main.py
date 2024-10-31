f7896bb15de515356b37868719e66001cbbdfdb1
AndrewAMur <andrewamuratov@gmail.com>
2024-10-30 22:10:57 -0400

    works only when there are at most three same suits in five cards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 
 suits = ["c", "d", "h", "s"]
 values = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -8,7 +9,7 @@ class Card:
         self.value = val
         self.suit = suit
     def show(self):
-        print(self.suit, self.value)
+        return (self.suit, self.value)
 
 def make_deck():
     deck = []
@@ -31,6 +32,34 @@ def choose_five():
 
     return chosen_cards
 
+def permutate(array):
+    return [list(permutation) for permutation in itertools.permutations(array)]
 
-for i in range(5):
+def sort_variations(all_permutation):
+    return sorted(all_permutation)[::-1]
+
+def validate(five):
+    first = five[0].value
+    last = five[-1].value
+    middle = [card.value for card in five[1:4]]
+    sorted_middle = sort_variations(middle)
+
+    for i in range(len(sorted_middle)):
+        if middle == sorted_middle:
+            increment = i + 1
+            if ((first + increment) % 12 == last) and (five[0].suit == five[-1].suit):
+                return True
+    return False
+
+def main():
     five_cards = choose_five()
+    permutations =  permutate(five_cards)
+    for variation in permutations:
+        if validate(variation):
+            print(f"Deck: {[i.show() for i in variation]}")
+            print(f"Ordered four cards (top to bottom){[i.show() for i in variation[:4]]}")
+            print(f"Hidden card: {variation[-1].show()}")
+            break
+
+if __name__ == "__main__":
+    main()
